34a9986d1ad41835f50f65c3db95f3388d35472c
Raphael Abdiche <abdiche.raphael@gmail.com>
2022-07-11 21:49:26 +0200

    modification du schéma user pour répondre aux demandes du cahier des chares

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -5,14 +5,6 @@ const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema(
     {
-        pseudo: {
-            type: String,
-            required: true,
-            minLength: 3,
-            maxLength: 55,
-            unique: true,
-            trim: true
-          },
           email: {
             type: String,
             required: true,
@@ -34,12 +26,6 @@ const userSchema = new mongoose.Schema(
         bio :{
             type: String,
             max: 1024,
-        },
-        followers: {
-            type: [String]
-        },
-        following: {
-            type: [String]
         },
         likes: {
             type: [String]
