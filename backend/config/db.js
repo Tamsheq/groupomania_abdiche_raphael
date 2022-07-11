6dd2d4149e2a5b52eb25e1349848ca50e9b5137f
Raphael Abdiche <abdiche.raphael@gmail.com>
2022-07-11 17:24:44 +0200

    creation newUser avec signUp en methode post + creation du schema userSchema dans moongose + ajout package Validator

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+// connexion à la base de données
+
 mongoose
     .connect("mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.a9pfb.mongodb.net/groupomania",)
     .then(() => console.log('Connected to MongoDB'))
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
-require('dotenv').config({path: './config/.env'})
+const userRoutes = require('./routes/user.routes');
+require('dotenv').config({path: './config/.env'});
 require ('./config/db');
 const app = express();
 
+
+
+
+
+// routes
+app.use('/api/user', userRoutes);
+
+// server
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
 })
