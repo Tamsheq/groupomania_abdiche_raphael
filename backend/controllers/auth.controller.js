690ac100f93969b335e09ad67b8026711304a76e
Raphael Abdiche <abdiche.raphael@gmail.com>
2022-07-20 17:20:38 +0200

    exports user afin de masquer leur mdp et verifier leur id

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,21 +1,13 @@
 const UserModel = require('../models/user.model');
 
-// destructuring
+//
 module.exports.signUp = async (req, res) => {
     console.log(req.body);
-<<<<<<< HEAD
     const {pseudo, email, password } = req.body
 
     try {
         const user = await UserModel.create({pseudo, email, password });
         res.status(201).json({ user: user._id });
-=======
-    const {email, password} = req.body
-
-    try {
-        const user = await UserModel.create({email, password });
-        res.status(201).json({ user: user._id});
->>>>>>> ddc8b2c8a145f5e2242a40163cebfbd36e1feb34
     }
     catch(err) {
         res.status(200).send({ err });
